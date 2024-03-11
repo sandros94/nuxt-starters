3daa51f590861b67c2dc18b39c3e0225e31783da
Sandros94 <sandro.circi@digitoolmedia.com>
2024-03-11 12:53:07 +0100

    chore(v3-base): init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@vueuse/nuxt',
-    '@nuxt/ui',
-  ],
-  ui: {
-    icons: {}
-  }
+  devtools: { enabled: true }
 })
