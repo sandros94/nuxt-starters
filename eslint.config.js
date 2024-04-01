dbdb70d5dd8770df6c7a988cf221eee6ed76cbd8
Sandros94 <sandro.circi@digitoolmedia.com>
2024-04-01 14:03:45 +0200

    feat(v3-base): switch to stefanobartoletti's eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+import { nuxt, stefanobartoletti, tailwind } from '@stefanobartoletti/eslint-config'
+
+export default stefanobartoletti(
+  {
+    ignores: [
+      // Project Specific
+    ],
+  },
+  nuxt,
+  tailwind,
+  {
+    // ESlint Flat config rule object
+    rules: {
+      'vue/html-self-closing': 'off',
+      'vue/no-multiple-template-root': 'off',
+      'unused-imports/no-unused-vars': 'warn',
+      'no-console': 'warn',
+
+      // Project Specific
+      'tailwindcss/no-custom-classname': 'off',
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+)
