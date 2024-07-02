d924934eef5da165c7f971fbc8a3d316716502b7
Sandros94 <sandro.circi@digitoolmedia.com>
2024-07-02 21:48:29 +0200

    feat: v4 template

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  extends: ['gh:sandros94/ui/main#b730806'],
+
   modules: [
     '@nuxt/eslint',
-    '@vueuse/nuxt',
-    '@nuxt/ui',
   ],
+
   eslint: {
     config: {
       stylistic: true,
     },
-  },
-  ui: {
-    icons: {},
   },
 })
