007beef338f008bb7f4617b85956f0ddcfc00239
Sandros94 <sandro.circi@digitoolmedia.com>
2024-08-29 09:45:42 +0200

    fix(module): eslint config indentation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,21 +16,21 @@ export default createConfigForNuxt({
   },
 })
   .append(
-  {
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'warn',
-      '@typescript-eslint/prefer-ts-expect-error': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      'vue/max-attributes-per-line': ['error', {
-        singleline: {
-          max: 4,
-        },
-        multiline: {
-          max: 1,
-        },
-      }],
+    {
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/prefer-ts-expect-error': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        'vue/max-attributes-per-line': ['error', {
+          singleline: {
+            max: 4,
+          },
+          multiline: {
+            max: 1,
+          },
+        }],
 
       // Project specific overrides
+      },
     },
-  },
   )
