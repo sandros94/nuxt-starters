1d8db966230bb0c65e73b213f69e1a7923ab5e00
Sandros94 <sandro.circi@digitoolmedia.com>
2025-01-08 13:34:26 +0100

    chore!: remove layer and fix deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,24 @@
 // https://nuxt.com/docs/guide/directory-structure/nuxt.config#nuxt-config-file
 export default defineNuxtConfig({
-
-  extends: ['gh:sandros94/ui/main-v3#5a4c08cbd8c1459a5356a30c763b835ec4833b44'],
-
   modules: [
     '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxtjs/mdc',
   ],
+
+  devtools: { enabled: true },
+
+  css: [
+    'assets/css/main.css',
+  ],
+
+  future: {
+    compatibilityVersion: 4,
+  },
 
   eslint: {
     config: {
       stylistic: true,
     },
-  },
-
-  devtools: { enabled: true },
-
-  future: {
-    compatibilityVersion: 4,
   },
 })
